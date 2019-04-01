0f257f9454740278fda4e29f891bc417678d4c54
Fred Song <fredsong1972@hotmail.com>
2019-03-31 23:20:58 -0700

    adding route search

--- MapMe/ViewController.swift
+++ MapMe/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import TomTomOnlineSDKMaps
+import TomTomOnlineSDKSearch
+import TomTomOnlineSDKRouting
+import TomTomOnlineSDKMapsUIExtensions
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, TTMapViewDelegate, TTAnnotationDelegate, TTAlongRouteSearchDelegate {
+    @IBOutlet weak var tomtomMap: TTMapView!
+    let route = TTRoute()
+    let reverseGeocoder = TTReverseGeocoder()
+    let alongRouteSearch = TTAlongRouteSearch()
+    var fullRoute: TTFullRoute?
+    var departurePosition = kCLLocationCoordinate2DInvalid
+    var destinationPosition = kCLLocationCoordinate2DInvalid
+    var wayPointPosition = kCLLocationCoordinate2DInvalid
+    var departureImage: TTAnnotationImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.initTomTomService()
+        self.initUIViews()
     }
-
-
+    
+    func initTomTomService(){
+        self.tomtomMap.delegate = self
+        self.tomtomMap.annotationManager.delegate = self
+        self.alongRouteSearch.delegate = self
+    }
+    
+    func initUIViews(){
+        self.departureImage = TTAnnotationImage.createPNG(withName: "ic_map_route_departure")
+        
+    }
+    
+    func clearMap(){
+        self.tomtomMap.routeManager.removeAllRoutes()
+        self.tomtomMap.annotationManager.removeAllAnnotations()
+        self.departurePosition = kCLLocationCoordinate2DInvalid
+        self.destinationPosition = kCLLocationCoordinate2DInvalid
+        self.wayPointPosition = kCLLocationCoordinate2DInvalid
+        self.fullRoute = nil
+    }
+    
 }
-
